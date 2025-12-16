b46527f4f311b0b59f0edf6efdd2a8fb7e059fa0
ckdduf138 <ckdduf138@naver.com>
2025-12-17 01:02:40 +0900

    fix: 폰트 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,7 +40,7 @@ export default {
         }
       },
       fontFamily: {
-        sans: ['MissedGyuwon', '-apple-system', 'BlinkMacSystemFont', 'Pretendard', 'Inter', 'system-ui', 'sans-serif'],
+        sans: ['Yeonseong', '-apple-system', 'BlinkMacSystemFont', 'Pretendard', 'Inter', 'system-ui', 'sans-serif'],
       },
       animation: {
         'fade-in': 'fadeIn 0.5s ease-in',
